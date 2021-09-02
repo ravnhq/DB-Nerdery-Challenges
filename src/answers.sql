098d8aab25c3ebbd8c119786e9aeb071f27edb5d
Victor Benavente <templario0141@gmail.com>
2021-09-02 00:54:21 -0500

    exercises 7,8 challenge completed

--- src/answers.sql
+++ src/answers.sql
@@ -67,6 +67,51 @@ GROUP BY
     o.name
 ORDER BY
     count(e.id) DESC;
-    
--- 7
 
+-- 7
+(
+    SELECT
+        o.address,
+        count(e.id) AS number_of_employees
+    FROM
+        offices o
+        INNER JOIN employees e ON o.id = e.office_id
+    GROUP BY
+        o.address
+    ORDER BY
+        COUNT(e.id) DESC
+    LIMIT
+        1
+)
+UNION
+(
+    SELECT
+        o.address,
+        COUNT(e.id) AS number_of_employees
+    FROM
+        offices o
+        INNER JOIN employees e ON o.id = e.office_id
+    GROUP BY
+        o.address
+    ORDER BY
+        COUNT(e.id) ASC
+    LIMIT
+        1
+);
+
+--8
+SELECT
+    e.uuid,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    e.email,
+    e.job_title,
+    o.name AS company,
+    c.name AS country,
+    s.name AS state,
+    b.first_name AS boss_name 
+FROM
+    employees e
+    INNER JOIN offices o ON e.office_id = o.id
+    INNER JOIN countries c ON o.country_id = c.id
+    INNER JOIN states s ON o.state_id = s.id
+    INNER JOIN employees b ON e.supervisor_id = b.id;
