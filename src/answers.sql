8f56ad6b22cc0ac6efc16e3058c463e8d41a8ac2
rdev32 <rdevelops32@gmail.com>
2023-05-15 00:00:04 -0500

    feat: add final solution

--- src/answers.sql
+++ src/answers.sql
@@ -58,6 +58,83 @@ ORDER BY total_after_mov DESC LIMIT 3;
 
 -- 5
 
+BEGIN;
+
+CREATE FUNCTION final_ammount_from(s_account_id UUID) RETURNS DOUBLE PRECISION
+LANGUAGE plpgsql AS
+$$
+    DECLARE result DOUBLE PRECISION;
+    BEGIN
+        WITH accounts_funds AS (
+            SELECT A.id,
+                   sum(B.mount) AS accounts_total
+            FROM users A
+            INNER JOIN accounts B ON A.id = B.user_id
+            WHERE A.id = s_account_id
+            GROUP BY A.id
+        ),
+        movements_funds AS (
+            SELECT  A.id,
+                    CONCAT(A.name, ' ', A.last_name) AS full_name,
+                    SUM(
+                       CASE
+                           WHEN C.account_to = B.id THEN C.mount
+                           WHEN C.type = 'OTHER' OR
+                                C.type = 'TRANSFER' OR
+                                C.type = 'OUT' THEN -1 * C.mount
+                           ELSE C.mount
+                       END) AS movements_total
+            FROM users A
+            INNER JOIN accounts B ON A.id = B.user_id
+            INNER JOIN movements C ON B.id = C.account_from OR B.id = C.account_to
+            WHERE B.id = s_account_id
+            GROUP BY A.name, A.last_name, A.id
+        )
+        SELECT A.id,
+               A.full_name,
+               A.movements_total,
+               B.accounts_total,
+               (B.accounts_total + A.movements_total) AS total_after_mov
+        INTO result
+        FROM movements_funds A
+        INNER JOIN accounts_funds B ON B.id = A.id
+        GROUP BY A.id, A.full_name, A.movements_total, B.accounts_total
+        ORDER BY total_after_mov DESC;
+
+        RETURN result;
+    END;
+$$;
+-- a
+SELECT final_ammount_from('3b79e403-c788-495a-a8ca-86ad7643afaf') as first_user,
+       final_ammount_from('fd244313-36e5-4a17-a27c-f8265bc46590') as second_user;
+-- b
+INSERT INTO movements (id, type, account_from, account_to, mount)
+VALUES (gen_random_uuid(), 'TRANSFER', '3b79e403-c788-495a-a8ca-86ad7643afaf', 'fd244313-36e5-4a17-a27c-f8265bc46590', 50.75);
+-- c
+DO $$
+    DECLARE
+        usr_acc_mount DOUBLE PRECISION;
+    BEGIN
+        SELECT final_ammount_from('3b79e403-c788-495a-a8ca-86ad7643afaf') INTO usr_acc_mount;
+        IF usr_acc_mount >= 731823.56
+            THEN
+                INSERT INTO movements (id, account_from, account_to, type, mount)
+                    VALUES (gen_random_uuid(), '3b79e403-c788-495a-a8ca-86ad7643afaf', null, 'OUT', 731823.56);
+            ELSE
+-- d
+                BEGIN
+                    RAISE EXCEPTION 'Not enough money';
+-- e
+                EXCEPTION
+                    WHEN OTHERS THEN RAISE;
+                END;
+        END IF;
+    END
+$$;
+-- f
+COMMIT;
+-- e
+SELECT final_ammount_from('fd244313-36e5-4a17-a27c-f8265bc46590') as second_user_final;
 
 -- 6
 SELECT * FROM movements A
